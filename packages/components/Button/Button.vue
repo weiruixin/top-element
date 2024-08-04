9db9622684b49b8f7347bd07c788c67031d5630b
weiruixin <986937726@qq.com>
2024-08-04 15:35:43 +0800

    :teda: first commit

--- packages/components/Button/Button.vue
+++ packages/components/Button/Button.vue
@@ -5,5 +5,5 @@ export default {
 </script>
 <script setup lang="ts"></script>
 <template>
-  <button style="color: red">test button</button>
+  <button style="color: red">test New button</button>
 </template>
