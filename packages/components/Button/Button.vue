42810686f769d1163b2216ac8d8dc0462bdf2f53
weiruixin <986937726@qq.com>
2024-08-04 14:34:32 +0800

    :teda: first commit

---
+++ packages/components/Button/Button.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  name: "ErButton",
+};
+</script>
+<script setup lang="ts"></script>
+<template>
+  <button style="color: red">test button</button>
+</template>
---
+++ packages/core/components.ts
@@ -0,0 +1,3 @@
+import { ErButton } from "@toy-element/components";
+import type {  Plugin} from "vue";
+export default [ErButton]as Plugin[]
